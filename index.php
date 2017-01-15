97c05ed7bee6fdbb475614c586c547de55a0917a
jenn <natapunch7@gmail.com>
2017-01-15 09:32:18 +0000

    push to github

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
 <?php
+echo 'YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY';
 
-echo "Hello!";
+require 'Classes/Person.php';
+require ('Classes/Student.php');
+require ('Classes/Prepod.php');
+require ('Classes/group.php');
+require ('Classes/Interf.php');
+
+$stud=new Student();
+$stud->speciality = 'Кибернетика';
+$stud->facultet = 'Экономика';
+$stud->group='A45';
+
+$prep=new Prepod();
+$prep->discipline = 'Физика';
+echo $stud->display_info_stud();
+echo $prep->display_info_prepod();
+?>
+
+
+
+
+
